10a119548afa780eea6ad8b94bfeb455cfedd106
Ahmed Alsaqaf <ahmadalsaggaf30@gmail.com>
2021-06-11 10:18:38 -0400

    add pAequorFactory()

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Returns a random DNA base
+const dnaBases = ['A', 'T', 'C', 'G'];
+const testDna = ['A', 'T', 'C', 'G', 'A', 'T', 'C', 'G', 'A', 'T', 'C', 'G', 'A', 'T', 'C'];
+
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -14,9 +16,45 @@ const mockUpStrand = () => {
 };
 
 
+const pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum,
+    dna,
+    mutate() {
+      const randdomIndex = Math.floor(Math.random() * this.dna.length);
+      let newBase = returnRandBase();
+
+      while ( this.dna[randdomIndex] === newBase){
+        newBase = returnRandBase();
+      }
+      this.dna[randdomIndex] = newBase;
+      return this.dna
+  },
+
+  compareDNA(pAequorPassed) {
+    let pAequorCurrent = this.dna;
+    let count = 0;
+
+    for (let i in pAequorPassed ){
+      if (pAequorPassed[i] === pAequorCurrent[i]){
+        count++;
+      }
+    }
+    let precentage =  (count / pAequorCurrent.length)* 100;
+    let precentageTodeci = precentage.toFixed();
+
+    console.log(`specimen #${this.specimenNum} and specimen #2 have ${precentageTodeci}% DNA in common`);
 
 
+  },
 
+  willLikelySurvive(){
+    const newObj = this.dna.filter(e => e === 'C' || e ==='G');
+    return newObj / this.dna.length >= 0.6;
+  }
 
+ }
+};
 
-
+// Test function
+const pAequor = pAequorFactory(1, mockUpStrand())
